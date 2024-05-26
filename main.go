f737bc81f1dafe801e5b954e695a37726e24d88b
leanghok120 <leanghokoeng5@gmail.com>
2024-05-27 05:39:40 +0700

    Add comment

--- main.go
+++ main.go
@@ -37,11 +37,10 @@ func getTime(format string) string {
 	currentTime := time.Now()
 
 	if format == "12" {
-		return currentTime.Format("03:04:05 PM")
+		return currentTime.Format("03:04:05 PM") // Returns the current time in HH:MM:SS AM/PM
 	} else if format == "" || format == "24" {
-		return currentTime.Format("15:04:05")
+		return currentTime.Format("15:04:05") // Returns the current time in HH:MM:SS
 	}
-
 	return "Invalid time format"
 }
 
