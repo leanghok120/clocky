cc21c7e563d26ff33d58984f189bcd0908ad7850
leanghok120 <leanghokoeng5@gmail.com>
2024-05-27 19:46:24 +0700

    Fix windows command propmt not clearing bug

--- pkg/clocky/term.go
+++ pkg/clocky/term.go
@@ -7,6 +7,12 @@ import (
 
 func ClearTerm() {
 	cmdName := "clear"
+
+	// Detect if on windows and run "cls" instead
+	if os.Getenv("OS") == "Windows_NT" {
+		cmdName = "cls"
+	}
+
 	cmd := exec.Command(cmdName)
 	cmd.Stdout = os.Stdout
 	cmd.Run()
